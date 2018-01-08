8b5d61a4e724b1cc4fe94019a56d7442763c5976
jingyus <382561874@qq.com>
2018-01-08 22:44:53 +0800

    numpy2.py
    learn numpy

---
+++ numpy2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  6 16:42:12 2018
+
+@author: Administrator
+"""
+import numpy as np
+
+a=np.arange(2,14).reshape(3,4)
+
+print(a)
+print(np.argmax(a))
+print(np.mean(a))
+print(np.cumsum(a))
+print(np.diff(a))
+print(np.nonzero(a))
